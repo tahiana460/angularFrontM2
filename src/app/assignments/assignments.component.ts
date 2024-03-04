e882891a7021964cb8705d8ef8392461ad2fe297
Tahiana <tahianarazakanome@gmail.com>
2024-03-04 15:39:55 +0300

    some updates

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RenduDirective } from '../shared/rendu.directive';
 
 @Component({
   selector: 'app-assignments',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RenduDirective],
   templateUrl: './assignments.component.html',
   styleUrl: './assignments.component.css'
 })
 export class AssignmentsComponent {
   titre = "Ceci est un titre xD"
+  assignments = [
+    {
+      nom: "TP1 sur WebComponents, un lecteur audio amélioré",
+      dateDeRendu: '2020-11-17',
+      rendu: true
+    },
+    {
+      nom: "TP2 sur Angular, un joli gestionnaire de devoirs (Assignments)",
+      dateDeRendu: '2020-12-15',
+      rendu: false
+    },
+    {
+      nom: "TP3 sur Angular",
+      dateDeRendu: '2021-01-04',
+      rendu: false
+    }
+  ];
 }
---
+++ src/app/shared/rendu.directive.spec.ts
@@ -0,0 +1,8 @@
+import { RenduDirective } from './rendu.directive';
+
+describe('RenduDirective', () => {
+  it('should create an instance', () => {
+    const directive = new RenduDirective();
+    expect(directive).toBeTruthy();
+  });
+});
